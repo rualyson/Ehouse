93b43cda5579e931e9b8c6101a186784b3d90a95
(Falcone) <(netofalcone@gmail.com)>
2018-09-21 09:37:57 -0300

    commmit dia 21/09

--- ehouse_inicio/urls.py
+++
@@ -1,9 +0,0 @@
-#!python
-from django.urls import include, path
-from . import views
-
-# We are adding a URL called /home
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('anuncios', views.anuncios, name='anuncios'),
-]
