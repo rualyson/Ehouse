beec40154c4727e45063004d2b5235fb5472e7f9
rualysonn <rualyson@gmail.com>
2018-10-15 23:16:57 -0300

    visualização de detalhes

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect 
+from django.shortcuts import get_object_or_404
 from .forms import *
 from .models import *
 from .forms import ImovelForm
@@ -42,15 +43,11 @@ def edit_delete(request, id):
 
 @login_required(login_url="/accounts/login")
 def detalhes_anuncio(request, id):
-    
     imovel = Imovel.objects.get(id=id)
-    form = ImovelForm(request.POST or None, instance=imovel)
-    if form.is_valid():
-        form.save()
-        return redirect('product_list')
-    return render(request, 'catalogo/detalhes_anuncio.html', {'form': form})
+    context = {'imovel' : imovel}
+    return render(request, 'catalogo/detalhes_anuncio.html', context = context )
 
 def buscarCep(request):
     buscar = request.POST['cep']
-    imovel = Imovel.objects.filter(cep__contains= 'buscar')
+    imovel = Imovel.objects.filter(cep__contains= buscar)
     return render(request, 'catalogo/busca.html', {'imovel':imovel})
