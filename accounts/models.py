d6eadddb3ecbb0f1844bbe13db5cd437925f64fe
Juliana Nascimento <juliana.conceicao@dce.ufpb.br>
2018-10-16 11:03:48 -0300

    cadastro de User Profile

--- accounts/models.py
+++ accounts/models.py
@@ -6,22 +6,22 @@ from django.db import IntegrityError
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
     nome = models.CharField('Nome Completo', max_length = 150)
     idade = models.IntegerField('Idade:')
-    cpf = models.IntegerField('CPF:')
-    cep = models.CharField('CEP:', max_length=8)
-    cidade = models.CharField('Cidade:', max_length=150)
+    cpf = models.IntegerField('CPF:')    
+    cidade = models.CharField('Cidade:', max_length=150, blank=True)
+    cep = models.CharField('CEP:', max_length=8, blank=True)
     bio = models.TextField(blank=True)
 
     def __str__(self):
         return self.nome
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
+def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from accounts.forms import RegisterForm
 from django.contrib.auth.forms import UserCreationForm
 from accounts.forms import ProfileForm
 from .models import Profile
+from django.db import transaction
+
 
 #Create your views here
 def cadastro(request):
@@ -17,48 +20,54 @@ def cadastro(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('cadastro_cliente')
+            return redirect('home')
     else:
         form = RegisterForm()
     return render(request, template_name, {'form': form})
 
+
 def cadastro_cliente(request):
     template_name = 'usuarios/cadastro_cliente.html'
-    form = ProfileForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('/')
-    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('cadastro')
+    else:
+        form = ProfileForm()
     return render(request, template_name, {'form': form})
 
+@login_required
+@transaction.atomic
 def meucadastro(request):
     if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=request.user.profile)
-        if form.is_valid():
-            form.save()
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, ('Seu perfil foi atualizado!'))
             return redirect('home')
+        else:
+            messages.error(request, ('Por favor, preencha os dados corretamente!'))
     else:
-        user = request.user
-        profile = user.profile
-        form = ProfileForm(instance = profile)
-    args = {}
-    # args.update(csrf(request))
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'usuarios/meucadastro.html', {
+        'profile_form': profile_form
+    })
+    
 
-    args['form'] = form
-    return render_to_response('usuarios/meucadastro.html', args)
-
+@login_required
+@transaction.atomic
 def editarcadastro(request):
     if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=request.user.profile)
+        form = UserCreationForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('/meucadastro/')
+            messages.success(request, ('Seu perfil foi atualizado!'))
+            return redirect('home')
+        else:
+            messages.error(request, ('Por favor, preencha os dados corretamente!'))
     else:
-        user = request.user
-        profile = user.profile
-        form = ProfileForm(instance = profile)
-    args = {}
-    # args.update(csrf(request))
-
-    args['form'] = form
-    return render_to_response('usuarios/meucadastro.html', args)
+        form = UserCreationForm(instance=request.user)
+    return render(request, 'usuarios/editarcadastro.html', {
+        'form': form
+    })
