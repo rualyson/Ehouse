05f7c0833235c003d0f945e132064868ef869c98
(Falcone) <(netofalcone@gmail.com)>
2018-10-16 04:35:25 -0300

    paginação

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,19 +1,38 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 from .models import *
 from .forms import ImovelForm
 from django.contrib.auth.decorators import login_required
+from django.views import generic
 
 
-@login_required(login_url="/accounts/login")
-def categorias(request):
-       return render(request, 'base.html')
+class ImovelListView(generic.ListView):
 
-@login_required(login_url="/accounts/login")
-def product_list(request):
-       context = {
-              'imoveis': Imovel.objects.all()}
-       return render(request, 'catalogo/product_list.html', context)
+    queryset = Imovel.objects.all()
+    model = Imovel
+    template_name = 'catalogo/product_list.html'
+    context_object_name = 'imoveis'
+    paginate_by = 2
+
+product_list = ImovelListView.as_view()
+
+
+class CategoryListView(generic.ListView):
+
+    template_name = 'catalogo/category.html'
+    context_object_name = 'product_list'
+
+    def get_queryset(self):
+        return Imovel.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
+
+
+category = CategoryListView.as_view()
+
 
 @login_required(login_url="/accounts/login")
 def product_new(request):
