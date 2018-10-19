c1c042cec30266837068ed020ae8f0e979473485
(Falcone) <(netofalcone@gmail.com)>
2018-10-19 09:02:40 -0300

    Pesquisa

--- Ehouse/settings.py
+++ Ehouse/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'anuncio',
     'catalogo',
     'widget_tweaks',
+    'watson',
 
 ]
 
--- catalogo/views.py
+++ catalogo/views.py
@@ -5,14 +5,26 @@ from .forms import ImovelForm
 from django.contrib.auth.decorators import login_required
 from django.views import generic
 
+from watson import search as watson
+from django.db import models
+
 
 class ImovelListView(generic.ListView):
 
     queryset = Imovel.objects.all()
-    model = Imovel
+
     template_name = 'catalogo/product_list.html'
     context_object_name = 'imoveis'
     paginate_by = 2
+
+    def get_queryset(self):
+        queryset = Imovel.objects.all()
+        q = self.request.GET.get('q', '')
+        if q:
+            queryset = watson.filter(queryset, q)
+        return queryset
+
+
 
 product_list = ImovelListView.as_view()
 
@@ -36,11 +48,13 @@ category = CategoryListView.as_view()
 
 @login_required(login_url="/accounts/login")
 def product_new(request):
-    form = ImovelForm(request.POST, request.FILES)
     if request.method == 'POST':
+        form = ImovelForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('product_list')
+
+    form = ImovelForm()
     return render(request, 'catalogo/novo_anuncio.html', {'form': form})
 
 @login_required(login_url="/accounts/login")
