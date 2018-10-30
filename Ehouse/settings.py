ed2cbe251354ff42823f8899dfe8bea7dfa9effa
rualysonn <rualyson@gmail.com>
2018-10-30 01:07:41 -0300

    adição de formulário

--- Ehouse/settings.py
+++ Ehouse/settings.py
@@ -102,8 +102,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'catalogo', 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/upload')
+MEDIA_URL = '/catalogo/media/'
 
 LOGIN_URL = '/accounts/login' 
 LOGIN_REDIRECT_URL = '/'
